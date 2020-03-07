c6959905d62d1c0d0916e88bcf18c68b43a847e9
Austin Loos <hackchart@gmail.com>
2020-03-06 23:28:49 -0800

    Create export_funcs.py
    Began wireframing output module

---
+++ export_funcs.py
@@ -0,0 +1,8 @@
+# TODO: Create function that creates a csv file with user meal plan data
+def to_csv():
+    with open("Meal Plan.csv", "w") as f:
+        pass
+
+def to_txt():
+    with open("Meal Plan.txt", "w") as f:
+        pass
